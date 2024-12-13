10ae9c418ef4497c72307b31a3d2768dff0e3276
leodennett <144666684+leodennett@users.noreply.github.com>
2024-12-13 17:15:09 +0100

    Adjusted Lava

--- _LavaStuff/LavaDeath.gd
+++ _LavaStuff/LavaDeath.gd
@@ -16,6 +16,6 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method("touched_lava"):
 		body.touched_lava()
 	#AudioController.play_deathlava()
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(0.5).timeout
 	if body.has_method("player_died"):
 		body.player_died()
