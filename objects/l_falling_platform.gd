36984cbf1ca5f85378820c9fe5e5719e5f10d902
leodennett <144666684+leodennett@users.noreply.github.com>
2024-12-16 14:22:55 +0100

    Worked on layout

--- objects/l_falling_platform.gd
+++ objects/l_falling_platform.gd
@@ -2,14 +2,19 @@ extends Node3D
 
 @onready var collision_shape_3d: CollisionShape3D = $Area3D/CollisionShape3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $Area3D/AnimationTree
+
 
 func _ready() -> void:
-	$Area3D/AnimationPlayer.play("Static")
+	pass
+	#$Area3D/AnimationPlayer.play("Static")
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	$Area3D/AnimationPlayer.play("Falling")
-	#animation_tree.travel("Regenerate")
+	pass
+	#if body.has_method("collect_coin"):
+		#$Area3D/AnimationPlayer.play("Falling")
+		#animation_tree.travel("Falling")
 	
 #func back_to_static():
 	#animation_tree.travel("Static")
