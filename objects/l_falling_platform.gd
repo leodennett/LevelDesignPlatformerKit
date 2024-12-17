dd2dfc7b33ba90065e6e2cfcb5f24af13b38a414
leodennett <144666684+leodennett@users.noreply.github.com>
2024-12-17 20:00:28 +0100

    Added audio to falling platforms

--- objects/l_falling_platform.gd
+++ objects/l_falling_platform.gd
@@ -3,6 +3,8 @@ extends Node3D
 @onready var collision_shape_3d: CollisionShape3D = $Area3D/CollisionShape3D
 @onready var animation_player: AnimationPlayer = $Area3D/AnimationPlayer
 @onready var animation_tree: AnimationTree = $Area3D/AnimationTree
+@onready var sfx_falling_platform: AudioStreamPlayer3D = $SFX_FallingPlatform
+@onready var sfx_land_fp: AudioStreamPlayer3D = $SFX_FallingPlatform/SFX_LandFP
 
 
 func _ready() -> void:
@@ -13,8 +15,11 @@ func _ready() -> void:
 func _on_area_3d_body_entered(body: Node3D):
 	
 	if body.has_method("collect_coin"):
+		$SFX_FallingPlatform/SFX_LandFP.play()
+		await get_tree().create_timer(0.5).timeout
 		print("Bloooob")
 		$Area3D/AnimationPlayer.play("Falling")
+		$SFX_FallingPlatform.play()
 		#animation_tree.travel("Falling")
 	
 #func back_to_static():
